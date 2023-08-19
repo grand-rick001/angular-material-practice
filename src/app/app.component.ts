9e97c86a1033e572ff3898e6a82233850c67aafa
Patrick Murimi <kabugap02@gmail.com>
2023-08-20 00:37:20 +0300

    [learn]: progress spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'material-code';
-  notifications = 2;
+  spinnerValue = 0;
 
-  addNotification() {
-    this.notifications++;
+  ngOnInit() {
+    setInterval(() => {
+      this.spinnerValue = this.spinnerValue + 1;
+      if (this.spinnerValue > 100) {
+        this.spinnerValue = 0;
+      }
+    }, 100);
   }
+  
 }
