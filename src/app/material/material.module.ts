81fc0c08331c58b02b4b840d1b6c4e3be3b03d18
Patrick Murimi <kabugap02@gmail.com>
2023-08-19 17:10:49 +0300

    [learn]: MatButtonToggle

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 const MaterialComponents = [
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatButtonToggleModule
 ]
 
 @NgModule({
