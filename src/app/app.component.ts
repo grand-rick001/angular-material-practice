cc79c8e812fb5db1ca4a297a67b808536b6b1708
Patrick Murimi <kabugap02@gmail.com>
2023-08-19 18:12:34 +0300

    [learn]: MatBadge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-code';
+  notifications = 2;
+
+  addNotification() {
+    this.notifications++;
+  }
 }
