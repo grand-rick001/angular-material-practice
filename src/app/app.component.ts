e6ccef3e5c91422146e12a871cb3dfbd954fa3b4
Patrick Murimi <kabugap02@gmail.com>
2023-08-21 03:47:12 +0300

    [learn]: Tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-
+  selectedIndex: number | null = 0;
   ngOnInit() {}
+
+  logChange(index: number | null) {
+    this.selectedIndex = index;
+    console.log(index);
+  }
   
 }
